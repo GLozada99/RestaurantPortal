895f979ff1d741a235018970a0515ebd02a9b3bc
Mauricio Pacheco <mpacheco@applaudostudios.dev>
2022-03-29 15:33:08 -0600

    Make Role level field unique

--- authentication/models.py
+++ authentication/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Role(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    level = models.IntegerField()
+    level = models.IntegerField(unique=True)
 
     def __str__(self):
         return f'{self.name}, level: {self.level}'
