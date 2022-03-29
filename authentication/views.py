1520a70f9b014952e3214021edd7766e699d9aeb
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-29 18:15:19 -0400

    Create Views for User Role

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from authentication.models import Role, User
+from authentication.serializers import RoleSerializer, UserSerializer
+
+
+class RoleAPIView(generics.ListCreateAPIView):
+    """View to list and create Roles"""
+
+    queryset = Role.objects.all().order_by('id')
+    serializer_class = RoleSerializer
+
+
+class RoleAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """View to retrieve, update and delete Roles"""
+
+    queryset = Role.objects.all().order_by('id')
+    serializer_class = RoleSerializer
