7e25091ac7e63df80b0cf4da95e353a56ca85f75
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-29 18:32:58 -0400

    Modify Role Views to only provide GET list and retrieve functionality

--- authentication/views.py
+++ authentication/views.py
@@ -4,14 +4,14 @@ from authentication.models import Role, User
 from authentication.serializers import RoleSerializer, UserSerializer
 
 
-class RoleAPIView(generics.ListCreateAPIView):
+class RoleAPIView(generics.ListAPIView):
     """View to list and create Roles"""
 
     queryset = Role.objects.all().order_by('id')
     serializer_class = RoleSerializer
 
 
-class RoleAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+class RoleAPIDetailView(generics.RetrieveAPIView):
     """View to retrieve, update and delete Roles"""
 
     queryset = Role.objects.all().order_by('id')
