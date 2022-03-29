9f9af2dc727e910cd76830e2b56ba80f608e0928
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-29 18:03:17 -0400

    Create User serializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from authentication.models import Role
+from authentication.models import Role, User
 
 
-class BasicUserRoleSerializer(serializers.ModelSerializer):
+class RoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
         fields = ('id', 'name', 'level')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'role', 'password')
