7c8dae6244ad5288916ffe09723eecab81390c2d
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-29 16:45:53 -0400

    Create Role model and make migrations

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Role(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    level = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name}, level: {self.level}'
