129df567b84b472a72f7d80ca7d1aa4c6fe5cabf
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-29 18:21:36 -0400

    Create Views for User

--- authentication/views.py
+++ authentication/views.py
@@ -16,3 +16,17 @@ class RoleAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Role.objects.all().order_by('id')
     serializer_class = RoleSerializer
+
+
+class UserAPIView(generics.ListCreateAPIView):
+    """View to list and create Users"""
+
+    queryset = User.objects.all().order_by('id')
+    serializer_class = UserSerializer
+
+
+class UserAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """View to retrieve, update and delete Users"""
+
+    queryset = User.objects.all().order_by('id')
+    serializer_class = UserSerializer
