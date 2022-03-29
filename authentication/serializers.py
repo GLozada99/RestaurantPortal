2f4785eb8101ec7f45efe6501b545c1ed51cd292
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-29 18:22:23 -0400

    Add docstring to serializers

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -4,12 +4,15 @@ from authentication.models import Role, User
 
 
 class RoleSerializer(serializers.ModelSerializer):
+    """Serializer for Role"""
+
     class Meta:
         model = Role
         fields = ('id', 'name', 'level')
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """Serializer for User"""
     password = serializers.CharField(write_only=True)
 
     class Meta:
