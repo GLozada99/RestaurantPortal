be1c6cc517a3ce1b70d925ecc61231c07ef1320d
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-30 18:02:02 -0400

    Create test_restaurant_active_administrators_positive and test_restaurant_active_branches_positive

--- restaurant/tests/test_restaurant.py
+++ restaurant/tests/test_restaurant.py
@@ -22,7 +22,7 @@ class RestaurantAPITestCase(APITestCase):
 
     @get_portal_manager_token
     def test_create_restaurant(self, token):
-        """Test the creation of a category."""
+        """Test the creation of a restaurant."""
         url = reverse('restaurants:restaurant-list')
 
         response = self.client.post(
@@ -32,10 +32,34 @@ class RestaurantAPITestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_restaurant_without_auth(self):
-        """Test the creation of a category without auth."""
+        """Test the creation of a restaurant without auth."""
         url = reverse('restaurants:restaurant-list')
 
         response = self.client.post(
             url, self.restaurant_data, format='json'
         )
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    @get_portal_manager_token
+    def test_restaurant_active_administrators_positive(self, token):
+        """Test the that number for active administrators for Restaurant must
+        be positive."""
+        url = reverse('restaurants:restaurant-list')
+        self.restaurant_data['active_administrators'] = -5
+        response = self.client.post(
+            url, self.restaurant_data, format='json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    @get_portal_manager_token
+    def test_restaurant_active_branches_positive(self, token):
+        """Test the that number for active branches for Restaurant must
+        be positive."""
+        url = reverse('restaurants:restaurant-list')
+        self.restaurant_data['active_branches'] = -5
+        response = self.client.post(
+            url, self.restaurant_data, format='json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
