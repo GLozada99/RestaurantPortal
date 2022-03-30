cdabbdc0857b501cbd1012f07361292c5bc96505
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-29 20:00:21 -0400

    Create create_client and create_portal_manager methods

--- authentication/services.py
+++ authentication/services.py
@@ -2,13 +2,17 @@ from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from rest_framework import status
 
+from authentication.models import Role
+from authentication.serializers import UserSerializer
+from portal.settings import CLIENT_LEVEL, PORTAL_MANAGER_LEVEL
+
 User = get_user_model()
 
 
-class UserAPIService():
+class UserAPIService:
 
     @staticmethod
-    def create(serializer):
+    def create(serializer: UserSerializer) -> Response:
         user = User(
             username=serializer.validated_data['username'],
             role=serializer.validated_data['role'],
@@ -21,3 +25,17 @@ class UserAPIService():
         user.set_password(serializer.validated_data['password'])
         user.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @classmethod
+    def create_client(cls, serializer: UserSerializer) -> Response:
+        serializer.validated_data['role'] = Role.objects.filter(
+            level=CLIENT_LEVEL
+        )
+        return cls.create(serializer)
+
+    @classmethod
+    def create_portal_manager(cls, serializer: UserSerializer) -> Response:
+        serializer.validated_data['role'] = Role.objects.filter(
+            level=PORTAL_MANAGER_LEVEL
+        )
+        return cls.create(serializer)
