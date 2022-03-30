37984ed00eac37807da52b98eebf87065c8c1c20
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-30 16:40:52 -0400

    Create views for Restaurant

---
+++ restaurant/views/restaurant.py
@@ -0,0 +1,30 @@
+from rest_framework import generics
+
+from authentication.permissions import IsPortalManager
+from restaurant.models import Restaurant
+from restaurant.serializers import (DetailedRestaurantSerializer,
+                                    RestaurantSerializer, )
+
+
+class RestaurantAPIView(generics.ListAPIView):
+    """View to list and create Restaurants."""
+
+    queryset = Restaurant.objects.all().order_by('id')
+    permission_classes = [IsPortalManager]
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return RestaurantSerializer
+        return DetailedRestaurantSerializer
+
+
+class RestaurantAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """View to retrieve, update and delete Restaurants."""
+
+    queryset = Restaurant.objects.all().order_by('id')
+    permission_classes = [IsPortalManager]
+
+    def get_serializer_class(self):
+        if self.request.method in {'PUT', 'PATCH'}:
+            return RestaurantSerializer
+        return DetailedRestaurantSerializer
