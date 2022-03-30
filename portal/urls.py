3d9e7aef14ba44033be7da34feb481a9f0232192
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-30 11:54:32 -0400

    Add urls for FoodType and DeliveryType

--- portal/urls.py
+++ portal/urls.py
@@ -30,5 +30,20 @@ urlpatterns = [
             namespace='portal_managers'
         )
     ),
+    path(
+         'food-types/',
+         include(
+             'restaurant.urls.food_type',
+             namespace='food_types'
+         )
+    ),
+    path(
+        'delivery-types/',
+        include(
+            'restaurant.urls.delivery_type',
+            namespace='delivery_types'
+        )
+    ),
+
     path('auth/', include('authentication.urls.jwt', namespace='auth')),
 ]
