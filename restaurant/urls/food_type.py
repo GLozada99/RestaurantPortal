5561f40abcf05c11703b733fc18d20557330ba6f
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-30 16:43:18 -0400

    Fix typo in app_name for urls of food_type and delivery_type}

--- restaurant/urls/food_type.py
+++ restaurant/urls/food_type.py
@@ -3,7 +3,7 @@ from django.urls import path
 from restaurant.views.food_type import (FoodTypeAPIDetailView,
                                         FoodTypeAPIView,)
 
-app_name = 'authentication'
+app_name = 'restaurant'
 urlpatterns = [
     path('', FoodTypeAPIView.as_view(), name='food-type-list'),
     path('<pk>/', FoodTypeAPIDetailView.as_view(), name='food-type-detail'),
