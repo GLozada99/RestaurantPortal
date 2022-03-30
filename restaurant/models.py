c01054669515aa3b6e0b435c28ba14ab2c995e60
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-30 16:08:38 -0400

    Create restaurant model and make migrations

--- restaurant/models.py
+++ restaurant/models.py
@@ -13,3 +13,15 @@ class DeliveryType(models.Model):
 
     def __str__(self):
         return f'DeliveryType: {self.name}'
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    food_type = models.ForeignKey(FoodType, on_delete=models.PROTECT)
+    active_branches = models.IntegerField()
+    active_administrators = models.IntegerField()
+    is_active = models.BooleanField()
+    delivery_types = models.ManyToManyField(DeliveryType)
+
+    def __str__(self):
+        return f'Restaurant: {self.name} - {self.food_type} - {self.is_active}'
