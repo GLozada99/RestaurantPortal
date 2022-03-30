e754633c085bd6093e3d3c6815326bfec0c768bc
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-30 17:56:16 -0400

    Create test_create_restaurant and test_create_restaurant_without_auth

--- restaurant/tests/test_restaurant.py
+++ restaurant/tests/test_restaurant.py
@@ -1,5 +1,9 @@
 from django.core.management import call_command
+from rest_framework import status
+from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
+
+from portal.test_helpers import get_portal_manager_token
 
 
 class RestaurantAPITestCase(APITestCase):
@@ -15,3 +19,23 @@ class RestaurantAPITestCase(APITestCase):
             'active_administrators': 5,
             'delivery_types': [1, 2]
         }
+
+    @get_portal_manager_token
+    def test_create_restaurant(self, token):
+        """Test the creation of a category."""
+        url = reverse('restaurants:restaurant-list')
+
+        response = self.client.post(
+            url, self.restaurant_data, format='json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_restaurant_without_auth(self):
+        """Test the creation of a category without auth."""
+        url = reverse('restaurants:restaurant-list')
+
+        response = self.client.post(
+            url, self.restaurant_data, format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
