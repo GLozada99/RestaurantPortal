8c76e5a5bbc07017e594c47c5e3588022a8a5056
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-30 10:28:00 -0400

    Create FoodType and DeliveryType models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class FoodType(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return f'FoodType: {self.name}'
+
+
+class DeliveryType(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return f'DeliveryType: {self.name}'
