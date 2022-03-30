32f9cf9f7c3885449e5ce24671c1bbccaecd534f
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-30 10:34:34 -0400

    Create views for DeliveryType

---
+++ restaurant/views/delivery_type.py
@@ -0,0 +1,21 @@
+from rest_framework import generics
+
+from authentication.permissions import IsPortalManager
+from restaurant.models import DeliveryType
+from restaurant.serializers import DeliveryTypeSerializer
+
+
+class DeliveryTypeAPIView(generics.ListAPIView):
+    """View to list DeliveryTypes."""
+
+    queryset = DeliveryType.objects.all().order_by('id')
+    serializer_class = DeliveryTypeSerializer
+    permission_classes = [IsPortalManager]
+
+
+class DeliveryTypeAPIDetailView(generics.RetrieveAPIView):
+    """View to retrieve DeliveryTypes."""
+
+    queryset = DeliveryType.objects.all().order_by('id')
+    serializer_class = DeliveryTypeSerializer
+    permission_classes = [IsPortalManager]
