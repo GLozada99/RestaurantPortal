26a2ecd29512a93bb6e39faeae337a20c77c23e4
Mauricio Pacheco <mpacheco@applaudostudios.dev>
2022-03-30 10:34:11 -0600

    Create createdeliverytypes command

---
+++ restaurant/management/commands/createdeliverytypes.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from portal.command_helpers import CommandHelpers
+from restaurant.models import DeliveryType
+
+
+class Command(BaseCommand):
+
+    help = ('A command to populate the delivery_type table.\n'
+            'This command does not need parameters')
+
+    @transaction.atomic
+    def handle(self, *args, **kwargs):
+        CommandHelpers.print_creating_message(self, 'DeliveryType', 3)
+        exists = DeliveryType.objects.filter(name='Delivery')
+        if exists:
+            CommandHelpers.print_error(self, 'Delivery types already exist.')
+        else:
+            delivery_types = [
+                DeliveryType(name='Delivery'),
+                DeliveryType(name='Take Out'),
+                DeliveryType(name='Eat Here'),
+            ]
+            CommandHelpers.add_to_db(self, DeliveryType, delivery_types)
+            CommandHelpers.print_success(self)
