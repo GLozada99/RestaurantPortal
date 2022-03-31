662f8cadd966acd2f20e50ebef21266bd6d5c5bc
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-03-31 11:55:12 -0400

    Remove redundant parentheses

--- portal/validators.py
+++ portal/validators.py
@@ -1,12 +1,12 @@
 from rest_framework.serializers import ValidationError
 
 
-class Validators():
+class Validators:
     """Class to handle validations."""
 
     @staticmethod
     def validate_greater_than_zero(value):
         """Validate that the entered value is greater than zero."""
-        if(value <= 0):
+        if value <= 0:
             raise ValidationError('This field must be greater than zero.')
         return value
