366a4e62b6cee1ee94b45110cba6a0ee0556a5de
Mauricio Pacheco <mpacheco@applaudostudios.dev>
2022-03-30 18:54:10 -0600

    Create DishCategory model and its migration

--- dish/models.py
+++ dish/models.py
@@ -1,8 +1,26 @@
 from django.db import models
 
+from restaurant.models import Restaurant
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=80)
-    
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class DishCategory(models.Model):
+    name = models.CharField(max_length=60)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'restaurant'],
+                name='restaurant_dish_category'
+            )
+        ]
+
     def __str__(self):
         return f'{self.name}'
