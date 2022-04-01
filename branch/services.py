aa2b20f5de6f42015aa52d3ea7ecbbf5b2dbdb66
Mauricio Pacheco <mpacheco@applaudostudios.dev>
2022-04-01 12:03:03 -0600

    Update BranchAPIService create method

--- branch/services.py
+++ branch/services.py
@@ -1,22 +1,13 @@
 from rest_framework import status
 from rest_framework.response import Response
 
-from branch.models import Branch
 from branch.serializers.branch import BranchSerializer
 
 
 class BranchAPIService:
 
     @classmethod
-    def create(
-        cls, serializer: BranchSerializer, restaurant_id: int
-    ) -> Response:
-        branch = Branch(
-            restaurant_id=restaurant_id,
-            address=serializer.validated_data['address'],
-            phone_number=serializer.validated_data['phone_number'],
-            front_picture=serializer.validated_data['front_picture'],
-        )
-        branch.save()
-        data = BranchSerializer(branch).data
-        return Response(data, status=status.HTTP_201_CREATED)
+    def create(cls, serializer: BranchSerializer, restaurant_id) -> Response:
+        serializer.validated_data['restaurant_id'] = restaurant_id
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
