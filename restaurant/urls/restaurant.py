c028778e9360926b3ae9c375c0cebb80d07b1729
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-01 00:00:27 -0400

    Set dish_category url inside restaurant url

--- restaurant/urls/restaurant.py
+++ restaurant/urls/restaurant.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 
 from restaurant.views.restaurant import (
     RestaurantAPIDetailView,
@@ -9,4 +9,14 @@ app_name = 'restaurant'
 urlpatterns = [
     path('', RestaurantAPIView.as_view(), name='restaurant-list'),
     path('<pk>/', RestaurantAPIDetailView.as_view(), name='restaurant-detail'),
+    path(
+        '<restaurant_pk>/dish-categories/',
+        path(
+            'dish-categories/',
+            include(
+                'dish.urls.dish_category',
+                namespace='dish-categories'
+            )
+        ),
+    )
 ]
