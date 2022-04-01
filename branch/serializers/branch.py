f4c643a24d481dcfd5441371fecbce1987d51de6
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-01 10:38:09 -0400

    Create serializer for Branch

---
+++ branch/serializers/branch.py
@@ -0,0 +1,14 @@
+from drf_extra_fields.fields import Base64ImageField
+from rest_framework import serializers
+
+from branch.models import Branch
+
+
+class BranchSerializer(serializers.ModelSerializer):
+    """Serializer for Branch."""
+    front_picture = Base64ImageField(
+        max_length=None, use_url=True)
+
+    class Meta:
+        model = Branch
+        fields = ('id', 'address', 'phone_number', 'front_picture')
