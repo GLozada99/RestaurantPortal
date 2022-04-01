472d8d83dcaa9198f3beab2be4e26f05cb24004f
Mauricio Pacheco <mpacheco@applaudostudios.dev>
2022-04-01 17:01:24 -0600

    Update Ingredient views to use IngredientAPIService
    Updated them because of the change in the urls

--- dish/views/ingredient.py
+++ dish/views/ingredient.py
@@ -2,17 +2,31 @@ from rest_framework import generics
 
 from dish.models import Ingredient
 from dish.serializers.ingredient import IngredientSerializer
+from dish.services.ingredient import IngredientAPIService
 
 
 class IngredientAPIView(generics.ListCreateAPIView):
     """View to list and create Ingredients."""
 
-    queryset = Ingredient.objects.all().order_by('id')
     serializer_class = IngredientSerializer
+
+    def get_queryset(self):
+        restaurant_id = self.kwargs.get('restaurant_id')
+        return Ingredient.objects.filter(restaurant__id=restaurant_id)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return IngredientAPIService.create(
+            serializer, self.kwargs.get('restaurant_id')
+        )
 
 
 class IngredientAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
     """View to retrieve, update and delete Ingredients."""
 
-    queryset = Ingredient.objects.all().order_by('id')
     serializer_class = IngredientSerializer
+
+    def get_queryset(self):
+        restaurant_id = self.kwargs.get('restaurant_id')
+        return Ingredient.objects.filter(restaurant__id=restaurant_id)
