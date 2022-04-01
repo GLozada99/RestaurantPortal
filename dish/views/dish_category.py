f739013310ebfd6b805d799904bb7687859f6d4f
Mauricio Pacheco <mpacheco@applaudostudios.dev>
2022-04-01 16:20:42 -0600

    Update DishCategory serializers and views

--- dish/views/dish_category.py
+++ dish/views/dish_category.py
@@ -1,10 +1,8 @@
 from rest_framework import generics
 
 from dish.models import DishCategory
-from dish.serializers.dish_category import (
-    DetailedDishCategorySerializer,
-    DishCategorySerializer,
-)
+from dish.serializers.dish_category import DishCategorySerializer
+from dish.services.dish_category import DishCategoryAPIService
 
 
 class DishCategoryAPIView(generics.ListCreateAPIView):
@@ -12,29 +10,22 @@ class DishCategoryAPIView(generics.ListCreateAPIView):
 
     serializer_class = DishCategorySerializer
 
-    def get_serializer_class(self):
-        if self.request.method == 'POST':
-            return DishCategorySerializer
-        return DetailedDishCategorySerializer
-
     def get_queryset(self):
         restaurant_id = self.kwargs.get('restaurant_id')
         return DishCategory.objects.filter(restaurant__id=restaurant_id)
 
     def post(self, request, *args, **kwargs):
-        request.data['restaurant'] = self.kwargs.get('restaurant_id')
-        return super().post(request, *args, **kwargs)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return DishCategoryAPIService.create(
+            serializer, self.kwargs.get('restaurant_id')
+        )
 
 
 class DishCategoryAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
     """View to retrieve, update and delete DishCategory."""
 
     serializer_class = DishCategorySerializer
-
-    def get_serializer_class(self):
-        if self.request.method in {'PUT', 'PATCH'}:
-            return DishCategorySerializer
-        return DetailedDishCategorySerializer
 
     def get_queryset(self):
         restaurant_id = self.kwargs.get('restaurant_id')
