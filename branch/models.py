794941b979b36bb8470627ab6053cbb23ae687d6
Mauricio Pacheco <mpacheco@applaudostudios.dev>
2022-04-01 12:07:46 -0600

    Make Branch front_picture field as nullable

--- branch/models.py
+++ branch/models.py
@@ -7,7 +7,9 @@ class Branch(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     address = models.TextField()
     phone_number = models.CharField(max_length=20)
-    front_picture = models.ImageField(upload_to='branch_front_pictures/')
+    front_picture = models.ImageField(
+        upload_to='branch_front_pictures/', null=True, blank=True
+    )
 
     def __str__(self):
         return f'{self.restaurant}\n{self.address}'
