efe6fa81dc936735c7adeb61fed8bfcf90c5687f
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-01 10:19:59 -0400

    Add AWS S3 related config to settings

--- portal/settings.py
+++ portal/settings.py
@@ -203,3 +203,15 @@ SWAGGER_SETTINGS = {
       }
    }
 }
+
+# Amazon S3 settings
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_ACCESS_KEY_ID = env_handler.AWS_S3_ACCESS_KEY_ID
+
+AWS_S3_SECRET_ACCESS_KEY = env_handler.AWS_S3_SECRET_ACCESS_KEY
+
+AWS_STORAGE_BUCKET_NAME = env_handler.AWS_STORAGE_BUCKET_NAME
+
+AWS_QUERYSTRING_AUTH = env_handler.AWS_QUERYSTRING_AUTH
