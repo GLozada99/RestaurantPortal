ce80bd2d94af68642752ddcb7e0de7cd04010038
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-01 00:01:09 -0400

    Override get_queryset method on DishCategory views

--- dish/views/dish_category.py
+++ dish/views/dish_category.py
@@ -10,13 +10,16 @@ from dish.serializers.dish_category import (
 class DishCategoryAPIView(generics.ListCreateAPIView):
     """View to list and create DishCategory."""
 
-    queryset = DishCategory.objects.all().order_by('id')
     serializer_class = DishCategorySerializer
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
             return DishCategorySerializer
         return DetailedDishCategorySerializer
+
+    def get_queryset(self):
+        restaurant_id = self.kwargs['restaurant_id']
+        return DishCategory.objects.filter(restaurant__id=restaurant_id)
 
 
 class DishCategoryAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
@@ -29,3 +32,7 @@ class DishCategoryAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
         if self.request.method in {'PUT', 'PATCH'}:
             return DishCategorySerializer
         return DetailedDishCategorySerializer
+
+    def get_queryset(self):
+        restaurant_id = self.kwargs['restaurant_id']
+        return DishCategory.objects.filter(restaurant__id=restaurant_id)
