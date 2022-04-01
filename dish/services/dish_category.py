decea3ba1073f6d74482fcef69299aa0942e63ec
Mauricio Pacheco <mpacheco@applaudostudios.dev>
2022-04-01 16:43:16 -0600

    Apply validate_unique to DishCategoryAPIService

--- dish/services/dish_category.py
+++ dish/services/dish_category.py
@@ -1,7 +1,9 @@
 from rest_framework import status
 from rest_framework.response import Response
 
+from dish.models import DishCategory
 from dish.serializers.dish_category import DishCategorySerializer
+from portal.validators import Validators
 
 
 class DishCategoryAPIService:
@@ -10,5 +12,14 @@ class DishCategoryAPIService:
     def create(
         cls, serializer: DishCategorySerializer, restaurant_id
     ) -> Response:
+        cls.validate_restaurant(
+            serializer.validated_data['name'], restaurant_id
+        )
         serializer.save(restaurant_id=restaurant_id)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @staticmethod
+    def validate_restaurant(name, restaurant_id):
+        Validators.validate_unique(
+            DishCategory, name=name, restaurant_id=restaurant_id
+        )
