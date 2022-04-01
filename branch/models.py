fdc00f8b877d18e8cfb4e4e024ad85b66f313b22
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-01 09:54:02 -0400

    Create Branch model

--- branch/models.py
+++ branch/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from restaurant.models import Restaurant
+
+
+class Branch(models.Model):
+    restaurant = models.ForeignKey(Restaurant)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=20)
+    front_picture_url = models.ImageField(upload_to='branch_front_pictures/')
+
+    def __str__(self):
+        return f'{self.restaurant}\n{self.address}'
