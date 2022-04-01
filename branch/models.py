02db0898fcb7e4fc2be658c730967d512c37e674
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-01 10:27:55 -0400

    Make migration for Branch model

--- branch/models.py
+++ branch/models.py
@@ -4,10 +4,10 @@ from restaurant.models import Restaurant
 
 
 class Branch(models.Model):
-    restaurant = models.ForeignKey(Restaurant)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     address = models.TextField()
     phone_number = models.CharField(max_length=20)
-    front_picture_url = models.ImageField(upload_to='branch_front_pictures/')
+    front_picture = models.ImageField(upload_to='branch_front_pictures/')
 
     def __str__(self):
         return f'{self.restaurant}\n{self.address}'
