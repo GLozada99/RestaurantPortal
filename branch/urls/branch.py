d0bbcb3c622f8cd6ccfef0ad266334482bbb87d2
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-02 14:43:53 -0400

    Add branch managers and employees urlpatterns to branch urlpatterns

--- branch/urls/branch.py
+++ branch/urls/branch.py
@@ -1,10 +1,24 @@
-from django.urls import path, include
+from django.urls import include, path
 
 from branch.views.branch import BranchAPIDetailView, BranchAPIView
 
 app_name = 'branch'
 urlpatterns = [
     path('', BranchAPIView.as_view(), name='branch-list'),
+    path(
+        '<branch_id>/managers/',
+        include(
+            'authentication.urls.branch_manager',
+            namespace='branch-managers'
+        )
+    ),
+    path(
+        '<branch_id>/employees/',
+        include(
+            'authentication.urls.employee',
+            namespace='employees'
+        )
+    ),
     path(
         '<pk>/',
         BranchAPIDetailView.as_view(),
