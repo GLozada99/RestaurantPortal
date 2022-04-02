2ed5d2c35b7f14b0f6ae29da4f02d23e3f724506
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-02 16:37:49 -0400

    Add token methods on test_helpers

--- portal/test_helpers.py
+++ portal/test_helpers.py
@@ -38,3 +38,51 @@ def get_client_token(f):
         ).data['access']
         return f(*args, **kwargs, token=token)
     return wrapper
+
+
+def get_restaurant_manager_token(f):
+    """This decorator only works if there's at least one restaurant created"""
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        serializer = UserSerializer(data=user_data)
+        restaurant_id = 1
+        UserAPIService.create_restaurant_manager(serializer, restaurant_id)
+        self = args[0]
+        token_url = reverse('auth:auth')
+        token = self.client.post(
+            token_url, user_data, format='json'
+        ).data['access']
+        return f(*args, **kwargs, token=token)
+    return wrapper
+
+
+def get_employee_token(f):
+    """This decorator only works if there's at least one branch created"""
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        serializer = UserSerializer(data=user_data)
+        branch_id = 1
+        UserAPIService.create_restaurant_manager(serializer, branch_id)
+        self = args[0]
+        token_url = reverse('auth:auth')
+        token = self.client.post(
+            token_url, user_data, format='json'
+        ).data['access']
+        return f(*args, **kwargs, token=token)
+    return wrapper
+
+
+def get_branch_manager_token(f):
+    """This decorator only works if there's at least one restaurant created"""
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        serializer = UserSerializer(data=user_data)
+        branch_id = 1
+        UserAPIService.create_branch_manager(serializer, branch_id)
+        self = args[0]
+        token_url = reverse('auth:auth')
+        token = self.client.post(
+            token_url, user_data, format='json'
+        ).data['access']
+        return f(*args, **kwargs, token=token)
+    return wrapper
