b7ff53d8080a14205279e9a58847b83eb15ec145
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-03 13:53:16 -0400

    Create test_delete_dish_category

--- dish/tests/test_dish_category.py
+++ dish/tests/test_dish_category.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
+from dish.models import DishCategory
 from portal.test_helpers import (get_branch_manager_token,
                                  get_restaurant_manager_token, )
 from restaurant.models import Restaurant
@@ -46,3 +47,37 @@ class DishCategoryAPITestCase(APITestCase):
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    @get_restaurant_manager_token
+    def test_delete_dish_category(self, token):
+        """Test the deletion of a dish category."""
+        call_command('createdishcategories')
+        url_get = reverse(
+            'restaurants:dish-categories:dish-category-list',
+            kwargs={'restaurant_id': Restaurant.objects.all().first().id}
+        )
+        response_get = self.client.get(
+            url_get, format='json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
+        )
+        current_categories = len(response_get.data)
+
+        url_delete = reverse(
+            'restaurants:dish-categories:dish-category-detail',
+            kwargs={
+                'restaurant_id': Restaurant.objects.all().first().id,
+                'pk': DishCategory.objects.all().first().id
+            }
+        )
+
+        self.client.delete(
+            url_delete, format='json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
+        )
+
+        response_get = self.client.get(
+            url_get, format='json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
+        )
+
+        self.assertEqual(current_categories, len(response_get.data) + 1)
