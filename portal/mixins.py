8879f8818f7e513615f73bd2032c15a29f4b7e12
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-03 22:30:46 -0400

    Create mixins module and create CheckRestaurantBranchAccordingMixin mixin

---
+++ portal/mixins.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from rest_framework import status
+
+from branch.models import Branch
+
+
+class CheckRestaurantBranchAccordingMixin(object):
+
+    @staticmethod
+    def are_restaurant_branch_according(
+            restaurant_id: int, branch_id: int):
+
+        branch = Branch.objects.get(pk=branch_id)
+        if branch.restaurant_id != restaurant_id:
+            return True
+
+    def dispatch(self, request, *args, **kwargs):
+        branch_id = self.kwargs.get('branch_id')
+        restaurant_id = self.kwargs.get('restaurant_id')
+        result = self.are_restaurant_branch_according(branch_id,
+                                                      restaurant_id)
+        if result:
+            return HttpResponse(
+                f'The branch with id {branch_id} does  not '
+                f'belong to the restaurant with id {restaurant_id}',
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        return super(CheckRestaurantBranchAccordingMixin, self).dispatch(
+            request,
+            *args,
+            **kwargs
+        )
