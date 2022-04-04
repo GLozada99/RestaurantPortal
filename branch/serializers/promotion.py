b02450a76ee143b39709632c1f992b9a87c32dae
Mauricio Pacheco <mpacheco@applaudostudios.dev>
2022-04-04 17:36:06 -0600

    Create Promotion serializers
    Created PromotionSerializer for create a promotion and
    DetailedPromotionSerializer to return a promotion

---
+++ branch/serializers/promotion.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+
+from branch.models import Promotion
+from branch.serializers.combo import ComboSerializer, DetailedComboSerializer
+from branch.serializers.branch import ShortBranchSerializer
+from portal.validators import Validators
+
+
+class PromotionSerializer(serializers.ModelSerializer):
+    """Serializer for Promotion."""
+
+    dishes = ComboSerializer(many=True)
+
+    class Meta:
+        model = Promotion
+        fields = (
+            'id',
+            'name',
+            'price',
+            'branches',
+            'dishes'
+        )
+
+    def validate_price(self, value):
+        return Validators.validate_greater_than_zero(value)
+
+
+class DetailedPromotionSerializer(serializers.ModelSerializer):
+    """Detailed Serializer for Promotion."""
+
+    branches = ShortBranchSerializer(many=True)
+    dishes = DetailedComboSerializer(many=True)
+
+    class Meta:
+        model = Promotion
+        fields = (
+            'id',
+            'name',
+            'price',
+            'branches',
+            'dishes'
+        )
