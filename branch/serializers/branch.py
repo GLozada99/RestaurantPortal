72caf30a18e90312662d42173f20d7deaf94cbf2
Mauricio Pacheco <mpacheco@applaudostudios.dev>
2022-04-04 17:35:26 -0600

    Create ShortBranchSerializer
    It only returns the id and address of a branch

--- branch/serializers/branch.py
+++ branch/serializers/branch.py
@@ -4,8 +4,17 @@ from rest_framework import serializers
 from branch.models import Branch
 
 
+class ShortBranchSerializer(serializers.ModelSerializer):
+    """Short Serializer for Branch."""
+
+    class Meta:
+        model = Branch
+        fields = ('id', 'address')
+
+
 class BranchSerializer(serializers.ModelSerializer):
     """Serializer for Branch."""
+
     front_picture = Base64ImageField(
         max_length=None, use_url=True, required=False
     )
