573a3cc1a57ace8227b66a7795dc82344cfccf9c
Mauricio Pacheco <mpacheco@applaudostudios.dev>
2022-04-04 12:06:34 -0600

    Refactor custom mixins

--- portal/mixins.py
+++ portal/mixins.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from rest_framework import status
 
 from branch.models import Branch
@@ -9,27 +9,28 @@ class CheckRestaurantBranchAccordingMixin(object):
 
     @staticmethod
     def are_restaurant_branch_according(
-            restaurant_id: int, branch_id: int):
-
+        restaurant_id: int, branch_id: int
+    ):
         branch = Branch.objects.get(pk=branch_id)
-        if branch.restaurant_id != restaurant_id:
-            return True
+        return branch.restaurant_id == restaurant_id
 
     def dispatch(self, request, *args, **kwargs):
         branch_id = int(self.kwargs.get('branch_id'))
         restaurant_id = int(self.kwargs.get('restaurant_id'))
-        result = self.are_restaurant_branch_according(branch_id,
-                                                      restaurant_id)
-        if result:
-            return HttpResponse(
-                f'The branch with id {branch_id} does not '
-                f'belong to the restaurant with id {restaurant_id}',
+        according = self.are_restaurant_branch_according(
+            restaurant_id, branch_id
+        )
+        if not according:
+            return JsonResponse(
+                {
+                    'detail':
+                    f'The branch with id {branch_id} does not '
+                    f'belong to the restaurant with id {restaurant_id}'
+                },
                 status=status.HTTP_400_BAD_REQUEST,
             )
         return super(CheckRestaurantBranchAccordingMixin, self).dispatch(
-            request,
-            *args,
-            **kwargs
+            request, *args, **kwargs
         )
 
 
@@ -37,25 +38,26 @@ class CheckRestaurantDishCategoryAccordingMixin(object):
 
     @staticmethod
     def are_restaurant_dish_category_according(
-            restaurant_id: int, dish_category_id: int):
-
+        restaurant_id: int, dish_category_id: int
+    ):
         dish_category = DishCategory.objects.get(pk=dish_category_id)
-        if dish_category.restaurant_id != restaurant_id:
-            return True
+        return dish_category.restaurant_id == restaurant_id
 
     def dispatch(self, request, *args, **kwargs):
         dish_category_id = int(self.kwargs.get('dish_category_id'))
         restaurant_id = int(self.kwargs.get('restaurant_id'))
-        result = self.are_restaurant_dish_category_according(dish_category_id,
-                                                             restaurant_id)
-        if result:
-            return HttpResponse(
-                f'The dish category with id {dish_category_id} does not '
-                f'belong to the restaurant with id {restaurant_id}',
+        according = self.are_restaurant_dish_category_according(
+            dish_category_id, restaurant_id
+        )
+        if not according:
+            return JsonResponse(
+                {
+                    'detail':
+                    f'The dish category with id {dish_category_id} does not '
+                    f'belong to the restaurant with id {restaurant_id}'
+                },
                 status=status.HTTP_400_BAD_REQUEST,
             )
         return super(CheckRestaurantDishCategoryAccordingMixin, self).dispatch(
-            request,
-            *args,
-            **kwargs
+            request, *args, **kwargs
         )
