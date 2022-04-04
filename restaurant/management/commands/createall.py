aac983fc21e027d557c88ad61447d33c8fff7f8f
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-04 09:47:52 -0400

    Implement createinventories command

--- restaurant/management/commands/createall.py
+++ restaurant/management/commands/createall.py
@@ -18,3 +18,4 @@ class Command(BaseCommand):
         call_command('createingredients')
         call_command('createdishes')
         call_command('createbranches')
+        call_command('createinventories')
