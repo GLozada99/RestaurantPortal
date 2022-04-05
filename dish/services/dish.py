65a9aae2dedfe54110c0ff90e6859cfb3bb4078c
Mauricio Pacheco <mpacheco@applaudostudios.dev>
2022-04-04 19:01:32 -0600

    Refactor DishAPIService

--- dish/services/dish.py
+++ dish/services/dish.py
@@ -1,7 +1,6 @@
 from django.db.transaction import atomic
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.serializers import ValidationError
 
 from dish.models import Dish, DishIngredient
 from dish.serializers.dish import DishSerializer
@@ -13,39 +12,40 @@ class DishAPIService:
     @classmethod
     @atomic
     def create(cls, serializer: DishSerializer, category_id: int) -> Response:
-        Validators.validate_unique(
-            Dish, name=serializer.validated_data['name'],
-            category_id=category_id,
+        ingredients_data = serializer.validated_data.pop('ingredients')
+        cls.validate_data(
+            serializer.validated_data['name'], category_id, ingredients_data
         )
-        dish = Dish(
-            name=serializer.validated_data['name'],
-            price=serializer.validated_data['price'],
-            description=serializer.validated_data['description'],
-            category_id=category_id,
+        serializer.save(category_id=category_id)
+        cls.create_dish_ingredients(serializer, ingredients_data)
+        cls.update_response_data(serializer, ingredients_data)
+        return Response(
+            DishSerializer(serializer.validated_data).data,
+            status=status.HTTP_201_CREATED
         )
-        dish.save()
-        ingredients_data = serializer.validated_data.get('ingredients')
-
-        dish_ingredients = [
-            DishIngredient(
-                dish_id=dish.id,
-                ingredient_id=data['ingredient'].id,
-                quantity=data['quantity'],
-                unit=data['unit'],
-            ) for data in ingredients_data
-        ]
-        cls.validate_dish_ingredients(dish_ingredients)
-        DishIngredient.objects.bulk_create(dish_ingredients)
-
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     @staticmethod
-    def validate_dish_ingredients(dish_ingredients):
-        ingredients = []
-        for dish_ingredient in dish_ingredients:
-            if dish_ingredient.ingredient_id in ingredients:
-                raise ValidationError({
-                    'non_field_errors':
-                        ['The fields ingredient, dish must make a unique set.']
-                })
-            ingredients.append(dish_ingredient.ingredient_id)
+    def create_dish_ingredients(serializer: DishSerializer, ingredients_data):
+        dish_ingredients = [
+            DishIngredient(
+                dish_id=serializer.data['id'],
+                ingredient_id=dish_ingredient['ingredient'].id,
+                quantity=dish_ingredient['quantity'],
+                unit=dish_ingredient['unit'],
+            ) for dish_ingredient in ingredients_data
+        ]
+        DishIngredient.objects.bulk_create(dish_ingredients)
+
+    @staticmethod
+    def update_response_data(serializer: DishSerializer, ingredients_data):
+        serializer.validated_data['ingredients'] = ingredients_data
+        serializer.validated_data['id'] = serializer.data['id']
+
+    @classmethod
+    def validate_data(cls, name, category_id, dish_ingredients):
+        Validators.validate_unique(
+            Dish, name=name, category_id=category_id,
+        )
+        Validators.validate_unique_id_in_list(
+            dish_ingredients, 'ingredient', 'dish'
+        )
