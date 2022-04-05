07cc9b03c5df477be4b4a176b8a9a4155e071bf0
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-05 14:20:34 -0400

    Add template endpoint for authentication

--- third_party_auth/urls/google.py
+++ third_party_auth/urls/google.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from third_party_auth.views.google import GoogleSocialAuthView
 
 app_name = 'google'
 urlpatterns = [
     path('', GoogleSocialAuthView.as_view()),
+    path('login/', TemplateView.as_view(
+        template_name='third_party_auth/google.html'))
 ]
