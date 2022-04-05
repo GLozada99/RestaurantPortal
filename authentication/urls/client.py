1587e8bf26e6198cc17090577e64c4938c89cb8d
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-05 12:19:35 -0400

    Fix app_name names authentication urls

--- authentication/urls/client.py
+++ authentication/urls/client.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from authentication.views.client import ClientAPIDetailView, ClientAPIView
 
-app_name = 'authentication'
+app_name = 'client'
 urlpatterns = [
     path('', ClientAPIView.as_view(), name='client-list'),
     path('<pk>/', ClientAPIDetailView.as_view(), name='client-detail'),
--- authentication/urls/restaurant_manager.py
+++ authentication/urls/restaurant_manager.py
@@ -3,7 +3,7 @@ from django.urls import path
 from authentication.views.restaurant_manager import (
     RestaurantManagerAPIDetailView, RestaurantManagerAPIView, )
 
-app_name = 'authentication'
+app_name = 'restaurant_manager'
 urlpatterns = [
     path('', RestaurantManagerAPIView.as_view(),
          name='restaurant-manager-list'),
