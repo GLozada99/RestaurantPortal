a10feb3b6ef1459202dc55848485f59795351513
Mauricio Pacheco <mpacheco@applaudostudios.dev>
2022-04-05 21:13:06 -0600

    Refactor test_restaurant_module

--- restaurant/tests/test_restaurant.py
+++ restaurant/tests/test_restaurant.py
@@ -3,67 +3,75 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITransactionTestCase
 
-from portal.test_helpers import (get_portal_manager_token,
-                                 )
-from restaurant.models import Restaurant
+from portal.test_helpers import get_portal_manager_token
 
 
 class RestaurantAPITestCase(APITransactionTestCase):
+
     reset_sequences = True
+
+    restaurant_data = {
+        'name': 'TestRestaurant',
+        'food_type': 1,
+        'active_branches': 5,
+        'active_administrators': 5,
+        'delivery_types': [1, 2]
+    }
+
+    restaurant_list_url = reverse('restaurants:restaurant-list')
 
     def setUp(self) -> None:
         call_command('createroles')
         call_command('createdeliverytypes')
         call_command('createfoodtypes')
         call_command('createrestaurants')
-        self.restaurant_data = {
-            'name': 'TestRestaurant',
-            'food_type': 1,
-            'active_branches': 5,
-            'active_administrators': 5,
-            'delivery_types': [1, 2]
-        }
 
     @get_portal_manager_token
     def test_create_restaurant(self, token):
         """Test the creation of a restaurant."""
-        url = reverse('restaurants:restaurant-list')
-
         response = self.client.post(
-            url, self.restaurant_data, format='json',
+            self.restaurant_list_url,
+            self.restaurant_data,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_restaurant_without_auth(self):
         """Test the creation of a restaurant without auth."""
-        url = reverse('restaurants:restaurant-list')
-
         response = self.client.post(
-            url, self.restaurant_data, format='json'
+            self.restaurant_list_url,
+            self.restaurant_data,
+            format='json'
         )
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     @get_portal_manager_token
     def test_restaurant_active_administrators_positive(self, token):
-        """Test the that number for active administrators for Restaurant must
-        be positive."""
-        url = reverse('restaurants:restaurant-list')
+        """
+        Test the that number for active administrators for Restaurant must be
+        positive.
+        """
         self.restaurant_data['active_administrators'] = -5
         response = self.client.post(
-            url, self.restaurant_data, format='json',
+            self.restaurant_list_url,
+            self.restaurant_data,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     @get_portal_manager_token
     def test_restaurant_active_branches_positive(self, token):
-        """Test the that number for active branches for Restaurant must
-        be positive."""
-        url = reverse('restaurants:restaurant-list')
+        """
+        Test the that number for active branches for Restaurant must be
+        positive.
+        """
         self.restaurant_data['active_branches'] = -5
         response = self.client.post(
-            url, self.restaurant_data, format='json',
+            self.restaurant_list_url,
+            self.restaurant_data,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
@@ -74,14 +82,16 @@ class RestaurantAPITestCase(APITransactionTestCase):
         call_command('createrestaurants')
         url = reverse(
             'restaurants:restaurant-managers:restaurant-manager-list',
-            kwargs={'restaurant_id': Restaurant.objects.all().first().id}
+            kwargs={'restaurant_id': 1}
         )
         manager_data = {
             'username': 'TestRestaurantManager',
             'password': 'TestPassword'
         }
         response = self.client.post(
-            url, manager_data, format='json',
+            url,
+            manager_data,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -90,31 +100,23 @@ class RestaurantAPITestCase(APITransactionTestCase):
     def test_delete_restaurant(self, token):
         """Test the deletion of a restaurant."""
         call_command('createrestaurants')
-
-        url_get = reverse(
-            'restaurants:restaurant-list',
-        )
         response_get = self.client.get(
-            url_get, format='json',
+            self.restaurant_list_url,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
         current_restaurants = len(response_get.data)
-
         url_delete = reverse(
-            'restaurants:restaurant-detail',
-            kwargs={
-                'pk': Restaurant.objects.all().first().id,
-            }
+            'restaurants:restaurant-detail', kwargs={'pk': 1}
         )
-
         self.client.delete(
-            url_delete, format='json',
+            url_delete,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
-
         response_get = self.client.get(
-            url_get, format='json',
+            self.restaurant_list_url,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
-
         self.assertEqual(current_restaurants, len(response_get.data) + 1)
