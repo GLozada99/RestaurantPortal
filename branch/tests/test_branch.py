fb6b53a9f8dc16074cd115507bc86e9f5eb206ae
Mauricio Pacheco <mpacheco@applaudostudios.dev>
2022-04-05 20:57:09 -0600

    Refactor test_branch module

--- branch/tests/test_branch.py
+++ branch/tests/test_branch.py
@@ -3,14 +3,25 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITransactionTestCase
 
-from branch.models import Branch
-from portal.test_helpers import (get_portal_manager_token,
-                                 get_restaurant_manager_token, )
-from restaurant.models import Restaurant
+from portal.test_helpers import (
+    get_portal_manager_token,
+    get_restaurant_manager_token,
+)
 
 
 class BranchAPITestCase(APITransactionTestCase):
+
     reset_sequences = True
+
+    manager_data = {
+        'username': 'TestManager',
+        'password': 'TestPassword'
+    }
+
+    branch_list_url = reverse(
+        'restaurants:branches:branch-list',
+        kwargs={'restaurant_id': 1}
+    )
 
     def setUp(self) -> None:
         call_command('createroles')
@@ -18,24 +29,18 @@ class BranchAPITestCase(APITransactionTestCase):
         call_command('createfoodtypes')
         call_command('createrestaurants')
         call_command('createbranches')
-        self.restaurant_id = Restaurant.objects.all().first().id
-        self.branch_id = Branch.objects.filter(
-            restaurant_id=self.restaurant_id
-        ).first().id
 
     @get_restaurant_manager_token
     def test_create_branch(self, token):
         """Test the creation of a restaurant branch."""
-        url = reverse(
-            'restaurants:branches:branch-list',
-            kwargs={'restaurant_id': self.restaurant_id}
-        )
         branch_data = {
             'address': 'TestAddress',
             'phone_number': '555-555-5555',
         }
         response = self.client.post(
-            url, branch_data, format='json',
+            self.branch_list_url,
+            branch_data,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -46,16 +51,14 @@ class BranchAPITestCase(APITransactionTestCase):
         url = reverse(
             'restaurants:branches:branch-managers:branch-manager-list',
             kwargs={
-                'restaurant_id': self.restaurant_id,
-                'branch_id': self.branch_id
+                'restaurant_id': 1,
+                'branch_id': 1
             }
         )
-        manager_data = {
-            'username': 'TestBranchManager',
-            'password': 'TestPassword'
-        }
         response = self.client.post(
-            url, manager_data, format='json',
+            url,
+            self.manager_data,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -66,16 +69,14 @@ class BranchAPITestCase(APITransactionTestCase):
         url = reverse(
             'restaurants:branches:branch-managers:branch-manager-list',
             kwargs={
-                'restaurant_id': self.restaurant_id,
-                'branch_id': self.branch_id
+                'restaurant_id': 1,
+                'branch_id': 1
             }
         )
-        manager_data = {
-            'username': 'TestBranchManager',
-            'password': 'TestPassword'
-        }
         response = self.client.post(
-            url, manager_data, format='json',
+            url,
+            self.manager_data,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
@@ -87,16 +88,14 @@ class BranchAPITestCase(APITransactionTestCase):
         url = reverse(
             'restaurants:branches:employees:employee-list',
             kwargs={
-                'restaurant_id': self.restaurant_id,
-                'branch_id': self.branch_id
+                'restaurant_id': 1,
+                'branch_id': 1
             }
         )
-        manager_data = {
-            'username': 'TestEmployee',
-            'password': 'TestPassword'
-        }
         response = self.client.post(
-            url, manager_data, format='json',
+            url,
+            self.manager_data,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -104,33 +103,27 @@ class BranchAPITestCase(APITransactionTestCase):
     @get_restaurant_manager_token
     def test_delete_branch(self, token):
         """Test the deletion of a branch."""
-
-        url_get = reverse(
-            'restaurants:branches:branch-list',
-            kwargs={
-                'restaurant_id': self.restaurant_id,
-            }
-        )
         response_get = self.client.get(
-            url_get, format='json',
+            self.branch_list_url,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
         current_branch = len(response_get.data)
-
         url_delete = reverse(
             'restaurants:branches:branch-detail',
             kwargs={
-                'restaurant_id': self.restaurant_id,
-                'pk': Branch.objects.all().first().id
+                'restaurant_id': 1,
+                'pk': response_get.data[0]['id']
             }
         )
         self.client.delete(
-            url_delete, format='json',
+            url_delete,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
         response_get = self.client.get(
-            url_get, format='json',
+            self.branch_list_url,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
-
         self.assertEqual(current_branch, len(response_get.data) + 1)
