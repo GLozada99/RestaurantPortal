b6ff158ccb532e5270ffe86056c355291d02444e
Mauricio Pacheco <mpacheco@applaudostudios.dev>
2022-04-05 21:02:28 -0600

    Refactor test_dish_category module

--- dish/tests/test_dish_category.py
+++ dish/tests/test_dish_category.py
@@ -3,14 +3,24 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITransactionTestCase
 
-from dish.models import DishCategory
-from portal.test_helpers import (get_branch_manager_token,
-                                 get_restaurant_manager_token, )
-from restaurant.models import Restaurant
+from portal.test_helpers import (
+    get_branch_manager_token,
+    get_restaurant_manager_token,
+)
 
 
 class DishCategoryAPITestCase(APITransactionTestCase):
+
     reset_sequences = True
+
+    category_data = {
+        'name': 'TestDishCategory',
+    }
+
+    dish_category_list_url = reverse(
+        'restaurants:dish-categories:dish-category-list',
+        kwargs={'restaurant_id': 1}
+    )
 
     def setUp(self) -> None:
         call_command('createroles')
@@ -22,32 +32,23 @@ class DishCategoryAPITestCase(APITransactionTestCase):
     @get_restaurant_manager_token
     def test_create_dish_category(self, token):
         """Test the creation of a dish category."""
-        url = reverse(
-            'restaurants:dish-categories:dish-category-list',
-            kwargs={'restaurant_id': Restaurant.objects.all().first().id}
-        )
-        category_data = {
-            'name': 'TestDishCategory',
-        }
         response = self.client.post(
-            url, category_data, format='json',
+            self.dish_category_list_url,
+            self.category_data,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     @get_branch_manager_token
     def test_create_dish_category_wrong_token(self, token):
-        """Test the creation of a dish category with branch
-        manager token.."""
-        url = reverse(
-            'restaurants:dish-categories:dish-category-list',
-            kwargs={'restaurant_id': Restaurant.objects.all().first().id}
-        )
-        category_data = {
-            'name': 'TestDishCategory',
-        }
+        """
+        Test the creation of a dish category with branch manager token.
+        """
         response = self.client.post(
-            url, category_data, format='json',
+            self.dish_category_list_url,
+            self.category_data,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
@@ -56,32 +57,27 @@ class DishCategoryAPITestCase(APITransactionTestCase):
     def test_delete_dish_category(self, token):
         """Test the deletion of a dish category."""
         call_command('createdishcategories')
-        url_get = reverse(
-            'restaurants:dish-categories:dish-category-list',
-            kwargs={'restaurant_id': Restaurant.objects.all().first().id}
-        )
         response_get = self.client.get(
-            url_get, format='json',
+            self.dish_category_list_url,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
         current_categories = len(response_get.data)
-
         url_delete = reverse(
             'restaurants:dish-categories:dish-category-detail',
             kwargs={
-                'restaurant_id': Restaurant.objects.all().first().id,
-                'pk': DishCategory.objects.all().first().id
+                'restaurant_id': 1,
+                'pk': response_get.data[0]['id']
             }
         )
-
         self.client.delete(
-            url_delete, format='json',
+            url_delete,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
-
         response_get = self.client.get(
-            url_get, format='json',
+            self.dish_category_list_url,
+            format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'}
         )
-
         self.assertEqual(current_categories, len(response_get.data) + 1)
