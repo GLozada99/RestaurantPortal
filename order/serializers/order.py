dbba6c22cae0a84e4e993135d923f20c6d5dfad3
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-07 13:52:25 -0400

    Create Order serializer

---
+++ order/serializers/order.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+
+from authentication.serializers.user import UserSerializer
+from branch.serializers.branch import ShortBranchSerializer
+from order.models import Order
+from order.serializers.order_dish import OrderDishSerializer
+from order.serializers.order_promotion import OrderPromotionSerializer
+from order.serializers.order_status import OrderStatusSerializer
+
+
+class CreateOrderSerializer(serializers.ModelSerializer):
+    """Serializer for Dish."""
+    dishes = OrderDishSerializer(many=True)
+    promotions = OrderPromotionSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = (
+            'address'
+            'dishes',
+            'promotions',
+        )
+
+
+class DetailedOrderSerializer(serializers.ModelSerializer):
+    """Detailed Serializer for Dish."""
+    client = UserSerializer()
+    status = OrderStatusSerializer()
+    branch = ShortBranchSerializer()
+
+    class Meta:
+        model = Order
+        fields = (
+            'client',
+            'status',
+            'branch',
+            'address'
+            'dishes',
+            'promotions',
+        )
