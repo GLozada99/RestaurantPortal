fad85763b3b8a749e0d22c2f617a3be6c8b47116
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-07 12:47:21 -0400

    Create Order, OrderDish and OrderPromotion models

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+from branch.models import Branch, Promotion
+from dish.models import Dish
+
+User = get_user_model()
 
 
 class OrderStatus(models.Model):
@@ -6,3 +12,40 @@ class OrderStatus(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+
+class Order(models.Model):
+    client = models.ForeignKey(User, on_delete=models.PROTECT)
+    status = models.ForeignKey(OrderStatus, on_delete=models.PROTECT)
+    branch = models.ForeignKey(Branch, on_delete=models.PROTECT)
+    dishes = models.ManyToManyField(Dish, through='OrderDish')
+    promotions = models.ManyToManyField(Promotion, through='OrderPromotion')
+    total_cost = models.FloatField()
+
+
+class OrderDish(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['order', 'dish'],
+                name='order_dish_unique_constraint',
+            ),
+        ]
+
+
+class OrderPromotion(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    promotion = models.ForeignKey(Promotion, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['order', 'promotion'],
+                name='order_promotion_unique_constraint',
+            ),
+        ]
