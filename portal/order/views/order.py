1bfd70e4d2859f65e6d3e9b7c96d9604478b4b39
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-07 19:23:42 -0400

    Create OrderAPIDetailView

---
+++ portal/order/views/order.py
@@ -0,0 +1,45 @@
+from rest_framework import generics
+
+from portal.authentication.permissions import (
+    HasCurrentBranch, IsBranchManager, IsClient, IsEmployee,
+)
+from portal.order.models import Order
+from portal.order.serializers.order import (CreateOrderSerializer,
+                                            DetailedOrderSerializer,
+                                            StatusOrderSerializer, )
+
+
+class OrderAPIView(generics.CreateAPIView):
+    """View to create Order."""
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CreateOrderSerializer
+        return DetailedOrderSerializer
+
+    def get_queryset(self):
+        branch_id = self.kwargs.get('branch_id')
+        return Order.objects.filter(branch_id=branch_id)
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsClient]
+        return [(IsBranchManager | IsEmployee) & HasCurrentBranch]
+
+
+class OrderAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """View to create Order."""
+
+    def get_serializer_class(self):
+        if self.request.method in {'PUT', 'PATCH'}:
+            return StatusOrderSerializer
+        return DetailedOrderSerializer
+
+    def get_queryset(self):
+        branch_id = self.kwargs.get('branch_id')
+        return Order.objects.filter(branch_id=branch_id)
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsClient]
+        return [(IsBranchManager | IsEmployee) & HasCurrentBranch]
