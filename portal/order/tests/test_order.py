224ae3d79de8b704929e18a07e986e86b27904b0
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-08 12:29:17 -0400

    Implement BranchAPITestCase and setUp method

---
+++ portal/order/tests/test_order.py
@@ -0,0 +1,15 @@
+from django.core.management import call_command
+from rest_framework.test import APITransactionTestCase
+
+
+class BranchAPITestCase(APITransactionTestCase):
+
+    reset_sequences = True
+
+    def setUp(self) -> None:
+        call_command('createroles')
+        call_command('createdeliverytypes')
+        call_command('createfoodtypes')
+        call_command('createrestaurants')
+        call_command('createbranches')
+        call_command('createinventories')
