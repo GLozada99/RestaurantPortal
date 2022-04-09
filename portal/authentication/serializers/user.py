f31ed91dd0312fb1ad27845ae1bd796c1357df81
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-09 14:02:35 -0400

    Set email required on UserSerializer

--- portal/authentication/serializers/user.py
+++ portal/authentication/serializers/user.py
@@ -5,7 +5,7 @@ from portal.authentication.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for User"""
-
+    email = serializers.EmailField(required=True)
     class Meta:
         model = User
         fields = (
@@ -23,7 +23,7 @@ class PasswordChangeSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
-            'email'
+            'email',
             'previous_password',
             'new_password',
         )
