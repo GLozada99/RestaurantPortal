aec3843f4ee8f40df8fc7ebf62d81ba7882a4cf1
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-08 20:29:50 -0400

    Create ShortOrderStatusSerializer and add previous_status to OrderStatusSerializer

--- portal/order/serializers/order_status.py
+++ portal/order/serializers/order_status.py
@@ -3,12 +3,25 @@ from rest_framework import serializers
 from portal.order.models import OrderStatus
 
 
-class OrderStatusSerializer(serializers.ModelSerializer):
-    """Serializer for OrderStatus"""
+class ShortOrderStatusSerializer(serializers.ModelSerializer):
+    """Short Serializer for OrderStatus"""
 
     class Meta:
         model = OrderStatus
         fields = (
             'id',
             'name',
+        )
+
+
+class OrderStatusSerializer(serializers.ModelSerializer):
+    """Serializer for OrderStatus"""
+    previous_status = ShortOrderStatusSerializer()
+
+    class Meta:
+        model = OrderStatus
+        fields = (
+            'id',
+            'name',
+            'previous_status'
         )
