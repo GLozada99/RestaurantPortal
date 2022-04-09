fbd25a98e78f48df8c1ee13ffc464e733159ae69
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-09 19:46:33 -0400

    Reformat BranchAPIService to not use Response

--- portal/branch/services/branch.py
+++ portal/branch/services/branch.py
@@ -1,14 +1,23 @@
-from rest_framework import status
-from rest_framework.response import Response
+from django.db.transaction import atomic
 
-from portal.branch.serializers.branch import BranchSerializer
-from portal.validators import Validators
+from portal.branch.models import Branch
 
 
 class BranchAPIService:
 
     @classmethod
-    def create(cls, serializer: BranchSerializer, restaurant_id) -> Response:
-        Validators.validate_create_new_branch(restaurant_id)
-        serializer.save(restaurant_id=restaurant_id)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    @atomic
+    def create(cls, data: dict, restaurant_id):
+        branch = cls.get_instance(data, restaurant_id)
+        return branch
+
+    @classmethod
+    def get_instance(cls, data: dict, restaurant_id: int) -> Branch:
+        branch = Branch(
+            restaurant_id=restaurant_id,
+            address=data['address'],
+            phone_number=data['phone_number'],
+            front_picture=data.get('front_picture'),
+        )
+        branch.save()
+        return branch
