3f4de4c708cdb6541b58472f2c53ae8bcf4cf593
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-10 11:55:33 -0400

    Reformat OrderAPIDetailView to use handler

--- portal/order/views/order.py
+++ portal/order/views/order.py
@@ -1,12 +1,6 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 
-from portal.authentication.permissions import (
-    HasCurrentBranch,
-    IsBranchManager,
-    IsClient,
-    IsEmployee,
-)
 from portal.order.handlers import OrderAPIHandler
 from portal.order.models import Order
 from portal.order.serializers.order import (
@@ -27,14 +21,14 @@ class OrderAPIView(generics.ListCreateAPIView):
         branch_id = self.kwargs.get('branch_id')
         return Order.objects.filter(branch_id=branch_id)
 
-    def get_permissions(self):
-        if self.request.method == 'POST':
-            self.permission_classes = [IsClient]
-        else:
-            self.permission_classes = [
-                (IsBranchManager | IsEmployee) & HasCurrentBranch
-            ]
-        return super().get_permissions()
+    # def get_permissions(self):
+    #     if self.request.method == 'POST':
+    #         self.permission_classes = [IsClient]
+    #     else:
+    #         self.permission_classes = [
+    #             (IsBranchManager | IsEmployee) & HasCurrentBranch
+    #         ]
+    #     return super().get_permissions()
 
     def post(self, request, *args, **kwargs):
         data = OrderAPIHandler.handle(
@@ -46,14 +40,20 @@ class OrderAPIView(generics.ListCreateAPIView):
 class OrderAPIDetailView(generics.RetrieveUpdateAPIView):
     """View to retrieve, update and delete Order."""
 
-    permission_classes = [(IsBranchManager | IsEmployee) & HasCurrentBranch]
-    http_method_names = ['get', 'put', 'delete', 'head', 'options', 'trace']
+    # permission_classes = [(IsBranchManager | IsEmployee) & HasCurrentBranch]
+    http_method_names = ['get', 'patch', 'delete', 'head', 'options', 'trace']
 
     def get_serializer_class(self):
-        if self.request.method == 'PUT':
+        if self.request.method == 'PATCH':
             return StatusOrderSerializer
         return ReadOrderSerializer
 
     def get_queryset(self):
         branch_id = self.kwargs.get('branch_id')
         return Order.objects.filter(branch_id=branch_id)
+
+    def patch(self, request, *args, **kwargs):
+        data = OrderAPIHandler.update_status(
+            request, kwargs.get('pk'),
+        )
+        return Response(data, status=status.HTTP_201_CREATED)
