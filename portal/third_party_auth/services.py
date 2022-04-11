a0171f463b66796ccc01df5979ba4677292e11f7
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-11 14:29:35 -0400

    Raise exception as guard clause on GoogleUserServices validate_token

--- portal/third_party_auth/services.py
+++ portal/third_party_auth/services.py
@@ -18,8 +18,9 @@ class GoogleUserServices:
     def validate_token(token: str):
         try:
             id_info = id_token.verify_oauth2_token(token, requests.Request())
-            if 'accounts.google.com' in id_info['iss']:
-                return id_info
+            if 'accounts.google.com' not in id_info['iss']:
+                raise ValueError
+            return id_info
         except GoogleAuthError:
             raise ValueError
 
