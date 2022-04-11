f60ea66618bfa26f042f759961b2ee10ec0a0f63
Mauricio Pacheco <mpacheco@applaudostudios.dev>
2022-04-10 21:34:00 -0600

    Update PromotionAPITestCase

--- portal/branch/tests/test_promotion.py
+++ portal/branch/tests/test_promotion.py
@@ -1,4 +1,5 @@
 from datetime import date
+import copy
 
 from django.core.management import call_command
 from rest_framework import status
@@ -55,14 +56,14 @@ class PromotionAPITestCase(APITransactionTestCase):
     @get_restaurant_manager_token
     def test_create_promotion_with_wrong_price(self, token):
         """Test the creation of a promotion with wrong price."""
-        self.promotion_data['price'] = 0
+        promotion_data = copy.deepcopy(self.promotion_data)
+        promotion_data['price'] = 0
         response = self.client.post(
             self.promotion_list_url,
-            self.promotion_data,
+            promotion_data,
             format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'},
         )
-        self.promotion_data['price'] = 9.99
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(
             response.data['price'][0],
@@ -72,14 +73,14 @@ class PromotionAPITestCase(APITransactionTestCase):
     @get_restaurant_manager_token
     def test_create_promotion_with_wrong_quantity(self, token):
         """Test the creation of a promotion with wrong quantity."""
-        self.promotion_data['dishes'][0]['quantity'] = 0
+        promotion_data = copy.deepcopy(self.promotion_data)
+        promotion_data['dishes'][0]['quantity'] = 0
         response = self.client.post(
             self.promotion_list_url,
-            self.promotion_data,
+            promotion_data,
             format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'},
         )
-        self.promotion_data['dishes'][0]['quantity'] = 2
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(
             response.data['dishes'][0]['quantity'][0],
@@ -89,27 +90,27 @@ class PromotionAPITestCase(APITransactionTestCase):
     @get_restaurant_manager_token
     def test_create_promotion_with_wrong_dish(self, token):
         """Test the creation of a promotion with a wrong dish."""
-        self.promotion_data['dishes'][0]['dish'] = 3
+        promotion_data = copy.deepcopy(self.promotion_data)
+        promotion_data['dishes'][0]['dish'] = 3
         response = self.client.post(
             self.promotion_list_url,
-            self.promotion_data,
+            promotion_data,
             format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'},
         )
-        self.promotion_data['dishes'][0]['dish'] = 1
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['dishes'], 'Invalid dishes.')
 
     @get_restaurant_manager_token
     def test_create_promotion_with_wrong_branch(self, token):
         """Test the creation of a promotion with wrong branch."""
-        self.promotion_data['branches'] = [2]
+        promotion_data = copy.deepcopy(self.promotion_data)
+        promotion_data['branches'] = [2]
         response = self.client.post(
             self.promotion_list_url,
-            self.promotion_data,
+            promotion_data,
             format='json',
             **{'HTTP_AUTHORIZATION': f'Bearer {token}'},
         )
-        self.promotion_data['branches'] = [1]
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['branches'], 'Invalid branches.')
