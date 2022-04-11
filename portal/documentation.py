ddd89da2e5dd4f82e6758e630d1355fb7ba6ea93
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-04-11 11:58:30 -0400

    Create BothHttpAndHttpsSchemaGenerator for documentation

--- portal/documentation.py
+++ portal/documentation.py
@@ -1,12 +1,22 @@
 from drf_yasg import openapi
+from drf_yasg.generators import OpenAPISchemaGenerator
 from drf_yasg.views import get_schema_view
 
+
+class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
+    def get_schema(self, request=None, public=False):
+        schema = super().get_schema(request, public)
+        schema.schemes = ["http", "https"]
+        return schema
+
+
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Restaurant portal API",
-      default_version='1.0.0',
-      description="API for restaurants",
-      terms_of_service="https://www.google.com/policies/terms/",
-   ),
-   public=True,
+    openapi.Info(
+        title="Restaurant portal API",
+        default_version='1.0.0',
+        description="API for restaurants",
+        terms_of_service="https://www.google.com/policies/terms/",
+    ),
+    generator_class=BothHttpAndHttpsSchemaGenerator,
+    public=True,
 )
